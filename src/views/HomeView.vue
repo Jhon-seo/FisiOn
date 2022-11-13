6cc46b51acc1b3e493016099ea816ca97ab69127
Jhonatan <75913125+Jhon-seo@users.noreply.github.com>
2022-11-13 16:12:29 -0400

    la primera vista creada

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HeroPrincipal from '../components/HeroPrincipal.vue'
+import Carrusel from "../components/Carrusel.vue"
+import InformacionPrimordial from '../components/InformacionPrimordial.vue'
+</script>
+
+<template>
+	<section>
+		<div>
+			<HeroPrincipal />
+		</div>
+		<Carrusel />
+		<InformacionPrimordial /> 
+	</section>
+</template>
