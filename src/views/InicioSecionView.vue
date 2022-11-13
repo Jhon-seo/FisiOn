1d5c2c17b4fad1667242af6c10597c75cbb8d583
Jhonatan <75913125+Jhon-seo@users.noreply.github.com>
2022-11-13 16:16:04 -0400

    vista del componente para iniciar seccion

---
+++ src/views/InicioSecionView.vue
@@ -0,0 +1,11 @@
+<script setup>
+ import InicioSecion from '../components/InicioSecion.vue'
+</script>
+
+<template>
+  <section class="columns mt-6">
+  	<div class="column"></div>
+  	<InicioSecion class="mt-6" />
+  	<div class="column"></div>
+  </section>
+</template>
