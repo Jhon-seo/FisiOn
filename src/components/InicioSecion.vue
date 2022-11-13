c82e9d8167d89def544413a1c95227ef17d7fa2b
Jhonatan <75913125+Jhon-seo@users.noreply.github.com>
2022-11-13 16:48:29 -0400

    El componente que permite iniciar seccion

---
+++ src/components/InicioSecion.vue
@@ -0,0 +1,39 @@
+<template>
+   <div class="is-half box fondo">
+	<div class="field pt-3 pb-4">
+<p class="subtitle ">Inicia Seccion</p>
+ <div class="column"></div>
+  <p class="control has-icons-left has-icons-right">
+    <input class="input" type="email" placeholder="Email">
+    <span class="icon is-small is-left">
+      <font-awesome-icon icon="fa-solid fa-at" />
+    </span>
+    <span class="icon is-small is-right">
+      
+    </span>
+  </p>
+</div>
+<div class="field">
+  <p class="control has-icons-left">
+    <input class="input is-success" type="password" placeholder="Password">
+    <span class="icon is-small is-left">
+      <font-awesome-icon icon="fa-solid fa-lock" />
+    </span>
+  </p>
+</div>
+<div class="field is-centred">
+  <p class="control">
+    <button class="button is-success">
+      Login
+    </button>
+  </p>
+</div>
+</div>
+</template>
+
+<style>
+.fondo{
+  background: #7be3fe;
+}
+
+</style>
